10f4c13a0b71a169d30defe4cc54c3ec6675426e
Washington Moreira <washington.moreira@gmail.com>
2022-12-07 15:57:25 -0300

    chore(AC-75): Fixed deploy code

--- entrypoint.sh
+++ entrypoint.sh
@@ -74,6 +74,7 @@ function deploy() {
 	printf "[\e[0;34mNOTICE\e[0m] Deploying $BRANCH to $WPENGINE_ENV.\n"
 
   cd "$GITHUB_WORKSPACE/../../$WPENGINE_ENVIRONMENT_NAME" && \
+  pwd && \
 	git add --all && \
 	git commit -m "GitHub Actions Deployment" && \
 	git status && \
@@ -90,5 +91,4 @@ function main() {
 	deploy
 }
 
-set -x
 main
