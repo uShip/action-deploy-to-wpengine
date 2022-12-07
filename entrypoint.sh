d8a544360c1ce5f396001e731163f6cd14b24f25
Washington Moreira <washington.moreira@gmail.com>
2022-12-07 15:42:06 -0300

    chore(AC-75): Fixed deploy code

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,14 +46,14 @@ function setup_private_key() {
 function clone_wpengine_repo() {
 	printf "[\e[0;34mNOTICE\e[0m] Cloning WPEngine's repository.\n"
 
-  cd "$GITHUB_WORKSPACE/.."
+  cd "$GITHUB_WORKSPACE/../.."
 	git clone --branch main git@$WPENGINE_HOST:$WPENGINE_ENV/$WPENGINE_ENVIRONMENT_NAME.git
 }
 
 function cleanup_wpengine_repo() {
 	printf "[\e[0;34mNOTICE\e[0m] Cleaning up WPEngine's repository.\n"
 
-	cd "$GITHUB_WORKSPACE/../$WPENGINE_ENVIRONMENT_NAME"
+	cd "$GITHUB_WORKSPACE/../../$WPENGINE_ENVIRONMENT_NAME"
 	rm -rf *
 }
 
@@ -73,13 +73,13 @@ function cleanup_local_repo() {
 function copy_local_repo_to_wpengine() {
 	printf "[\e[0;34mNOTICE\e[0m] Copying Local repo to WPEngine's repository.\n"
 
-  cp -r "$GITHUB_WORKSPACE/." "$GITHUB_WORKSPACE/../$WPENGINE_ENVIRONMENT_NAME/"
+  cp -r "$GITHUB_WORKSPACE/." "$GITHUB_WORKSPACE/../../$WPENGINE_ENVIRONMENT_NAME/"
 }
 
 function setup_remote_user() {
 	printf "[\e[0;34mNOTICE\e[0m] Setting up remote repository.\n"
 
-  cd "$GITHUB_WORKSPACE/../$WPENGINE_ENVIRONMENT_NAME"
+  cd "$GITHUB_WORKSPACE/../../$WPENGINE_ENVIRONMENT_NAME"
 	git config user.name "Automated Deployment"
 	git config user.email "automation@uship.com"
 }
